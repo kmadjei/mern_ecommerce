f5f401f67e61624eaf9f29f892f8bf602ec16358
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-03-26 16:15:19 -0400

    readding project files after 1hr tutorial of react ecommerce by lam

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`;
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`;
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
+
