b3a91d6fba44b065bc881529beda24384f841689
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-03-26 23:36:21 -0400

    completed tutorial on --> shopping product list ui, styled components hover & animations, react newsletter design, and react footer design

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,15 +1,21 @@
 import styled from "styled-components";
+import { popularProducts } from '../data';
+import Product from './Product';
 
 const Container = styled.div`
-    flex: 1;
-    margin: 3px;
-    height: 70vh;
-    position: relative;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <Container>
+      {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
   )
 }
 
