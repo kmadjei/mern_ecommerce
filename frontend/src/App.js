060f55aafe57e2ed9063cc7cb962ee02736859a5
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-03-27 08:59:07 -0400

    completed tutorial videos on -->Shopping Category page, and Eccommerce single product page';

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './pages/Product';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
-    <Home />
+    <Product />
   );
 }
 
